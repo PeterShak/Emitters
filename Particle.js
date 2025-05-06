4d10306f828cf181d3059add6e5111526c480bf3
PeterShak <peter_shaknes@avenues-ny.org>
2025-05-06 09:40:45 -0400

    Added colors to particles

--- Particle.js
+++ Particle.js
@@ -1,41 +1,36 @@
 class Particle {
-  constructor(x,y) {
-    this.position = createVector(x,y)
-    this.velocity = p5.Vector.random2D()
-    this.acceleration = p5.Vector.random2D()
+  constructor(x, y) {
+    this.position = createVector(x, y)
+    this.velocity = p5.Vector.random2D().mult(random(0.5, 2))
+    this.acceleration = createVector(0, 0)
     this.r = 10
     this.lifespan = 255
+
+    // Assign a random color
+    this.color = color(random(100, 255), random(100, 255), random(100, 255))
   }
-  
+
   update() {
     this.velocity.add(this.acceleration)
-    this.position.add(this.velocity)   
+    this.position.add(this.velocity)
     this.acceleration.mult(0)
     this.lifespan -= 2
   }
-  
+
   isDead() {
     return this.lifespan < 0
-    // if( this.lifespan < 0 ) {
-    //   return true 
-    // }
-    // return false
   }
-  
+
   draw() {
-    fill(255, this.lifespan)
-    circle(this.position.x,this.position.y,this.r)
+    fill(this.color)
+    circle(this.position.x, this.position.y, this.r)
   }
-  
-  // f is a p5.Vector
+
   applyForce(f) {
     this.acceleration.add(f)
   }
-  
-  
-  static createStandardParticleAt(x,y) {
-    return new Particle(
-      x,y
-    )
+
+  static createStandardParticleAt(x, y) {
+    return new Particle(x, y)
   }
 }
